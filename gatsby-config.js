92f20a34cf412c1fa6b8547f9f86f8c7bf3b2441
Munxdelger <70076584+Munxdelger@users.noreply.github.com>
2021-02-04 22:30:32 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
-        head: true,
-        anonymize: true,
+        head: false,
       },
     },
     {
